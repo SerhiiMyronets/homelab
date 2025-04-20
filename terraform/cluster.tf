65a1c9889873586274e32b159f81975b5cd70ced
SerhiiMyronets <hardmail88@gmail.com>
2025-04-19 23:22:23 -0700

    added base proxmox and talos terraform

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -6,62 +6,52 @@ data "talos_client_configuration" "talosconfig" {
   endpoints            = [var.talos_cp_01_ip_addr]
 }
 
-data "talos_machine_configuration" "machineconfig_cp" {
+resource "talos_cluster_kubeconfig" "kubeconfig" {
+  depends_on           = [talos_machine_bootstrap.bootstrap, data.talos_cluster_health.health]
+  client_configuration = talos_machine_secrets.machine_secrets.client_configuration
+  node                 = var.talos_cp_01_ip_addr
+}
+
+data "talos_machine_configuration" "controller" {
   cluster_name     = var.cluster_name
   cluster_endpoint = "https://${var.talos_cp_01_ip_addr}:6443"
   machine_type     = "controlplane"
   machine_secrets  = talos_machine_secrets.machine_secrets.machine_secrets
 }
 
-resource "talos_machine_configuration_apply" "cp_config_apply" {
-  depends_on                  = [ proxmox_virtual_environment_vm.talos_cp_01 ]
+resource "talos_machine_configuration_apply" "controller" {
+  depends_on                  = [proxmox_virtual_environment_vm.control_plane]
   client_configuration        = talos_machine_secrets.machine_secrets.client_configuration
-  machine_configuration_input = data.talos_machine_configuration.machineconfig_cp.machine_configuration
+  machine_configuration_input = data.talos_machine_configuration.controller.machine_configuration
   count                       = 1
   node                        = var.talos_cp_01_ip_addr
 }
 
-data "talos_machine_configuration" "machineconfig_worker" {
+data "talos_machine_configuration" "worker" {
   cluster_name     = var.cluster_name
   cluster_endpoint = "https://${var.talos_cp_01_ip_addr}:6443"
   machine_type     = "worker"
   machine_secrets  = talos_machine_secrets.machine_secrets.machine_secrets
 }
 
-resource "talos_machine_configuration_apply" "worker_config_apply" {
-  depends_on                  = [ proxmox_virtual_environment_vm.talos_worker_01 ]
+resource "talos_machine_configuration_apply" "worker" {
+  depends_on                  = [proxmox_virtual_environment_vm.talos_worker_01]
   client_configuration        = talos_machine_secrets.machine_secrets.client_configuration
-  machine_configuration_input = data.talos_machine_configuration.machineconfig_worker.machine_configuration
+  machine_configuration_input = data.talos_machine_configuration.worker.machine_configuration
   count                       = 1
   node                        = var.talos_worker_01_ip_addr
 }
 
 resource "talos_machine_bootstrap" "bootstrap" {
-  depends_on           = [ talos_machine_configuration_apply.cp_config_apply ]
+  depends_on           = [talos_machine_configuration_apply.controller]
   client_configuration = talos_machine_secrets.machine_secrets.client_configuration
   node                 = var.talos_cp_01_ip_addr
 }
 
 data "talos_cluster_health" "health" {
-  depends_on           = [ talos_machine_configuration_apply.cp_config_apply, talos_machine_configuration_apply.worker_config_apply ]
+  depends_on           = [talos_machine_configuration_apply.controller, talos_machine_configuration_apply.worker]
   client_configuration = data.talos_client_configuration.talosconfig.client_configuration
-  control_plane_nodes  = [ var.talos_cp_01_ip_addr ]
-  worker_nodes         = [ var.talos_worker_01_ip_addr ]
+  control_plane_nodes  = [var.talos_cp_01_ip_addr]
+  worker_nodes         = [var.talos_worker_01_ip_addr]
   endpoints            = data.talos_client_configuration.talosconfig.endpoints
-}
-
-data "talos_cluster_kubeconfig" "kubeconfig" {
-  depends_on           = [ talos_machine_bootstrap.bootstrap, data.talos_cluster_health.health ]
-  client_configuration = talos_machine_secrets.machine_secrets.client_configuration
-  node                 = var.talos_cp_01_ip_addr
-}
-
-output "talosconfig" {
-  value = data.talos_client_configuration.talosconfig.talos_config
-  sensitive = true
-}
-
-output "kubeconfig" {
-  value = data.talos_cluster_kubeconfig.kubeconfig.kubeconfig_raw
-  sensitive = true
 }
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,18 +1,26 @@
 terraform {
   required_providers {
     proxmox = {
-      source = "bpg/proxmox"
+      source  = "bpg/proxmox"
       version = "0.73.0"
     }
     talos = {
-      source = "siderolabs/talos"
+      source  = "siderolabs/talos"
       version = "0.7.1"
     }
   }
-   backend "s3" {
-    bucket         = "serhii-myronets"
-    key            = "homelab/cluster.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-   }
+  backend "s3" {
+    bucket  = "serhii-myronets"
+    key     = "homelab/cluster.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+}
+
+provider "proxmox" {
+
+  endpoint = "https://192.168.1.200:8006/"
+  username = "root@pam"
+  password = "tata3846"
+  insecure = true
 }
