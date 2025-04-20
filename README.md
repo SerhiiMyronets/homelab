5499f67ec6ad253aaa452c9ece7e91ce818e8028
SerhiiMyronets <hardmail88@gmail.com>
2025-04-19 22:17:43 -0700

    add git ignore

---
+++ README.md
@@ -0,0 +1,6 @@
+# My Homelab project
+
+This is my homelab project with talos linux and proxmox. The goal is to have a homelab with a few VMs and containers running on it.
+
+## 🚀 Proxmox setup
+- network setup /proxmox/interfaces > /etc/network/interfaces
---
+++ terraform/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+      version = "0.73.0"
+    }
+    talos = {
+      source = "siderolabs/talos"
+      version = "0.7.1"
+    }
+  }
+   backend "s3" {
+    bucket         = "serhii-myronets"
+    key            = "homelab/cluster.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+   }
+}
