6d56db248d5d90da55e63ab0e8442804009a0694
SerhiiMyronets <hardmail88@gmail.com>
2025-04-27 13:44:36 -0700

    updated to multi control plane config

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -3,55 +3,58 @@ resource "talos_machine_secrets" "machine_secrets" {}
 data "talos_client_configuration" "talosconfig" {
   cluster_name         = var.cluster_name
   client_configuration = talos_machine_secrets.machine_secrets.client_configuration
-  endpoints            = [var.talos_cp_01_ip_addr]
+  endpoints            = [for node in local.controller_nodes : node.address]
 }
 
 resource "talos_cluster_kubeconfig" "kubeconfig" {
   depends_on           = [talos_machine_bootstrap.bootstrap, data.talos_cluster_health.health]
   client_configuration = talos_machine_secrets.machine_secrets.client_configuration
-  node                 = var.talos_cp_01_ip_addr
+  node                 = local.controller_nodes[0].address
 }
 
 data "talos_machine_configuration" "controller" {
   cluster_name     = var.cluster_name
-  cluster_endpoint = "https://${var.talos_cp_01_ip_addr}:6443"
+  cluster_endpoint = "https://${local.controller_nodes[0].address}:6443"
   machine_type     = "controlplane"
   machine_secrets  = talos_machine_secrets.machine_secrets.machine_secrets
 }
 
 resource "talos_machine_configuration_apply" "controller" {
+  count                       = var.controller_config.count
   depends_on                  = [proxmox_virtual_environment_vm.control_plane]
   client_configuration        = talos_machine_secrets.machine_secrets.client_configuration
   machine_configuration_input = data.talos_machine_configuration.controller.machine_configuration
-  count                       = 1
-  node                        = var.talos_cp_01_ip_addr
+  endpoint                    = local.controller_nodes[count.index].address
+  node                        = local.controller_nodes[count.index].address
 }
 
 data "talos_machine_configuration" "worker" {
   cluster_name     = var.cluster_name
-  cluster_endpoint = "https://${var.talos_cp_01_ip_addr}:6443"
+  cluster_endpoint = "https://${local.controller_nodes[0].address}:6443"
   machine_type     = "worker"
   machine_secrets  = talos_machine_secrets.machine_secrets.machine_secrets
 }
 
 resource "talos_machine_configuration_apply" "worker" {
+  count                       = var.worker_config.count
   depends_on                  = [proxmox_virtual_environment_vm.talos_worker_01]
   client_configuration        = talos_machine_secrets.machine_secrets.client_configuration
   machine_configuration_input = data.talos_machine_configuration.worker.machine_configuration
-  count                       = 1
-  node                        = var.talos_worker_01_ip_addr
+  endpoint                    = local.worker_nodes[count.index].address
+  node                        = local.worker_nodes[count.index].address
 }
 
 resource "talos_machine_bootstrap" "bootstrap" {
   depends_on           = [talos_machine_configuration_apply.controller]
+  endpoint             = local.controller_nodes[0].address
   client_configuration = talos_machine_secrets.machine_secrets.client_configuration
-  node                 = var.talos_cp_01_ip_addr
+  node                 = local.controller_nodes[0].address
 }
 
 data "talos_cluster_health" "health" {
   depends_on           = [talos_machine_configuration_apply.controller, talos_machine_configuration_apply.worker]
   client_configuration = data.talos_client_configuration.talosconfig.client_configuration
-  control_plane_nodes  = [var.talos_cp_01_ip_addr]
-  worker_nodes         = [var.talos_worker_01_ip_addr]
+  control_plane_nodes  = local.controller_nodes[*].address
+  worker_nodes         = local.worker_nodes[*].address
   endpoints            = data.talos_client_configuration.talosconfig.endpoints
 }
