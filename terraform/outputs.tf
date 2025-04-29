e2c5e127802d1e6736ef7c1f8b5873a790c74fda
SerhiiMyronets <hardmail88@gmail.com>
2025-04-29 10:49:24 -0700

    refactoring

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,13 +3,12 @@
 // ==============================================================================
 
 output "talosconfig" {
-  description = <<-EOT
-    Talos client configuration (talosconfig) in raw format.
-    Save it locally by running:
-      terraform output -raw talosconfig > ~/.talos/config
-  EOT
   value     = data.talos_client_configuration.talosconfig.talos_config
   sensitive = true
+}
+
+output "kubeconfig_command" {
+  value = "terraform output -raw kubeconfig > ~/.kube/config"
 }
 
 // ==============================================================================
@@ -17,11 +16,11 @@ output "talosconfig" {
 // ==============================================================================
 
 output "kubeconfig" {
-  description = <<-EOT
-    Kubernetes kubeconfig for accessing the cluster.
-    Save it locally by running:
-      terraform output -raw kubeconfig > ~/.kube/config
-  EOT
+
   value     = talos_cluster_kubeconfig.kubeconfig.kubeconfig_raw
   sensitive = true
+}
+
+output "talosconfig_command" {
+  value = "terraform output -raw talosconfig > ~/.talos/config"
 }
